d76751c9e3f59bf11c174c636aaf2ce775652a16
Di Liu <unknowngas@gmail.com>
2017-03-31 01:46:57 -0400

    Test query

--- handler.py
+++ handler.py
@@ -1,27 +1,49 @@
-# get this file's directory independent of where it's run from
+#coding:utf-8
+from datetime import datetime
+from datetime import timedelta
+from __future__ import print_function
 import sys, os
+import xlsxwriter
+import boto3
+import json
+import time
 
-here = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(here, "./vendored"))
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
-import requests
-import logging
+# Send query request to 'readings' table
+def request_data(sensor_id, start_timestamp, end_timestamp):
+	dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
+	table = dynamodb.Table('readings')
 
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+	response = table.query(
+        TableName='readings',
+        ConsistentRead=True,
+        KeyConditionExpression='sensor_id = :sensor_id and published_at BETWEEN :start_timestamp AND :end_timestamp',
+        ExpressionAttributeValues={
+            ':sensor_id': {
+                'S': sensor_id,
+            },
+            ':start_timestamp': {
+                'N': str(start_timestamp)
+            },
+            ':end_timestamp': {
+                'N': str(end_timestamp)
+            },
+        },
+    )
+    return response
 
-API_HOST = 'https://jsonplaceholder.typicode.com'
 
-
-def list_posts(event, context):
-    url = API_HOST + '/posts'
-
-    log.debug('calling ' + url)
-    return requests.get(url).json()
-
-
-def get_post(event, context):
-    url = API_HOST + '/posts/' + event['path']['id']
-
-    log.debug('calling ' + url)
-    return requests.get(url).json()
+def make_xlsx(event, context):
+    print(event)
+    response = request_data('TEMPERATURE_DHT11_1', 1490135972113, 1490137092113)
+    print("Query succeeded:")
+    print(json.dumps(response, indent=4, cls=DecimalEncoder))
